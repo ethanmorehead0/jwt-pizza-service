249ec35e701c601534587d94414b744a2b76f11d
ethan <ethan0@byu.edu>
2025-03-18 11:14:19 -0600

    test metrics update

--- src/metrics.js
+++ src/metrics.js
@@ -1,4 +1,8 @@
 const os = require("os");
+const config = require("./config");
+
+let requests = 0;
+let latency = 0;
 
 function getCpuUsagePercentage() {
   const cpuUsage = os.loadavg()[0] / os.cpus().length;
@@ -13,19 +17,86 @@ function getMemoryUsagePercentage() {
   return memoryUsage.toFixed(2);
 }
 
-function reportMetrics() {
-  const cpuUsage = getCpuUsagePercentage();
-  const memoryUsage = getMemoryUsagePercentage();
-  console.log(`CPU Usage: ${cpuUsage}%`);
-  console.log(`Memory Usage: ${memoryUsage}%`);
+function sendMetricToGrafana(metricName, metricValue, type, unit) {
+  const metric = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics: [
+              {
+                name: metricName,
+                unit: unit,
+                [type]: {
+                  dataPoints: [
+                    {
+                      asInt: metricValue,
+                      timeUnixNano: Date.now() * 1000000,
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  if (type === "sum") {
+    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][
+      type
+    ].aggregationTemporality = "AGGREGATION_TEMPORALITY_CUMULATIVE";
+    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][
+      type
+    ].isMonotonic = true;
+  }
+
+  const body = JSON.stringify(metric);
+  fetch(`${config.url}`, {
+    method: "POST",
+    body: body,
+    headers: {
+      Authorization: `Bearer ${config.apiKey}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        response.text().then((text) => {
+          console.error(
+            `Failed to push metrics data to Grafana: ${text}\n${body}`
+          );
+        });
+      } else {
+        console.log(`Pushed ${metricName}`);
+      }
+    })
+    .catch((error) => {
+      console.error("Error pushing metrics:", error);
+    });
 }
 
 // Report metrics every 5 seconds
-setInterval(reportMetrics, 5000);
+setInterval(() => {
+  const cpuValue = Math.floor(Math.random() * 100) + 1;
+  sendMetricToGrafana("cpu", cpuValue, "gauge", "%");
+
+  requests += Math.floor(Math.random() * 200) + 1;
+  sendMetricToGrafana("requests", requests, "sum", "1");
+
+  latency += Math.floor(Math.random() * 200) + 1;
+  sendMetricToGrafana("latency", latency, "sum", "ms");
+}, 5000);
 
 function track(route) {
   return (req, res, next) => {
     console.log(`Tracking route: ${route}`);
+
+    const metricName = `route_${route}_requests`;
+    requests += 1;
+    sendMetricToGrafana(metricName, requests, "sum", "1");
+
     next();
   };
 }
