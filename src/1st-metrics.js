4bc76df141bdfcd527c22eb522a129d48ab42855
ethan <ethan0@byu.edu>
2025-03-18 10:45:01 -0600

    commented out old code

--- src/1st-metrics.js
+++ src/1st-metrics.js
@@ -1,113 +1,113 @@
-const os = require("os");
-const config = require("./config");
+// const os = require("os");
+// const config = require("./config");
 
-const requests = {};
+// const requests = {};
 
-function track(endpoint) {
-  return (req, res, next) => {
-    requests[endpoint] = (requests[endpoint] || 0) + 1;
-    next();
-  };
-}
+// function track(endpoint) {
+//   return (req, res, next) => {
+//     requests[endpoint] = (requests[endpoint] || 0) + 1;
+//     next();
+//   };
+// }
 
-// This will periodically send metrics to Grafana
-const timer = setInterval(() => {
-  Object.keys(requests).forEach((endpoint) => {
-    sendMetricToGrafana("requests", requests[endpoint], { endpoint });
-  });
-}, 10000);
+// // This will periodically send metrics to Grafana
+// const timer = setInterval(() => {
+//   Object.keys(requests).forEach((endpoint) => {
+//     sendMetricToGrafana("requests", requests[endpoint], { endpoint });
+//   });
+// }, 10000);
 
-function sendMetricToGrafana(metricName, metricValue, attributes) {
-  attributes = { ...attributes, source: config.source };
+// function sendMetricToGrafana(metricName, metricValue, attributes) {
+//   attributes = { ...attributes, source: config.source };
 
-  const metric = {
-    resourceMetrics: [
-      {
-        scopeMetrics: [
-          {
-            metrics: [
-              {
-                name: metricName,
-                unit: "1",
-                sum: {
-                  dataPoints: [
-                    {
-                      asInt: metricValue,
-                      timeUnixNano: Date.now() * 1000000,
-                      attributes: [],
-                    },
-                  ],
-                  aggregationTemporality: "AGGREGATION_TEMPORALITY_CUMULATIVE",
-                  isMonotonic: true,
-                },
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
+//   const metric = {
+//     resourceMetrics: [
+//       {
+//         scopeMetrics: [
+//           {
+//             metrics: [
+//               {
+//                 name: metricName,
+//                 unit: "1",
+//                 sum: {
+//                   dataPoints: [
+//                     {
+//                       asInt: metricValue,
+//                       timeUnixNano: Date.now() * 1000000,
+//                       attributes: [],
+//                     },
+//                   ],
+//                   aggregationTemporality: "AGGREGATION_TEMPORALITY_CUMULATIVE",
+//                   isMonotonic: true,
+//                 },
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//     ],
+//   };
 
-  Object.keys(attributes).forEach((key) => {
-    metric.resourceMetrics[0].scopeMetrics[0].metrics[0].sum.dataPoints[0].attributes.push(
-      {
-        key: key,
-        value: { stringValue: attributes[key] },
-      }
-    );
-  });
+//   Object.keys(attributes).forEach((key) => {
+//     metric.resourceMetrics[0].scopeMetrics[0].metrics[0].sum.dataPoints[0].attributes.push(
+//       {
+//         key: key,
+//         value: { stringValue: attributes[key] },
+//       }
+//     );
+//   });
 
-  fetch(`${config.url}`, {
-    method: "POST",
-    body: JSON.stringify(metric),
-    headers: {
-      Authorization: `Bearer ${config.apiKey}`,
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        console.error("Failed to push metrics data to Grafana");
-      } else {
-        console.log(`Pushed ${metricName}`);
-      }
-    })
-    .catch((error) => {
-      console.error("Error pushing metrics:", error);
-    });
-}
+//   fetch(`${config.url}`, {
+//     method: "POST",
+//     body: JSON.stringify(metric),
+//     headers: {
+//       Authorization: `Bearer ${config.apiKey}`,
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((response) => {
+//       if (!response.ok) {
+//         console.error("Failed to push metrics data to Grafana");
+//       } else {
+//         console.log(`Pushed ${metricName}`);
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error pushing metrics:", error);
+//     });
+// }
 
-module.exports = { track };
+// module.exports = { track };
 
-function getCpuUsagePercentage() {
-  const cpuUsage = os.loadavg()[0] / os.cpus().length;
-  return cpuUsage.toFixed(2) * 100;
-}
+// function getCpuUsagePercentage() {
+//   const cpuUsage = os.loadavg()[0] / os.cpus().length;
+//   return cpuUsage.toFixed(2) * 100;
+// }
 
-function getMemoryUsagePercentage() {
-  const totalMemory = os.totalmem();
-  const freeMemory = os.freemem();
-  const usedMemory = totalMemory - freeMemory;
-  const memoryUsage = (usedMemory / totalMemory) * 100;
-  return memoryUsage.toFixed(2);
-}
+// function getMemoryUsagePercentage() {
+//   const totalMemory = os.totalmem();
+//   const freeMemory = os.freemem();
+//   const usedMemory = totalMemory - freeMemory;
+//   const memoryUsage = (usedMemory / totalMemory) * 100;
+//   return memoryUsage.toFixed(2);
+// }
 
-function sendMetricsPeriodically(period) {
-  const timer = setInterval(() => {
-    try {
-      const buf = new MetricBuilder();
-      buf.addMetric("cpu_usage", getCpuUsagePercentage(), { unit: "%" });
-      buf.addMetric("memory_usage", getMemoryUsagePercentage(), { unit: "%" });
-      httpMetrics(buf);
-      systemMetrics(buf);
-      userMetrics(buf);
-      purchaseMetrics(buf);
-      authMetrics(buf);
+// function sendMetricsPeriodically(period) {
+//   const timer = setInterval(() => {
+//     try {
+//       const buf = new MetricBuilder();
+//       buf.addMetric("cpu_usage", getCpuUsagePercentage(), { unit: "%" });
+//       buf.addMetric("memory_usage", getMemoryUsagePercentage(), { unit: "%" });
+//       httpMetrics(buf);
+//       systemMetrics(buf);
+//       userMetrics(buf);
+//       purchaseMetrics(buf);
+//       authMetrics(buf);
 
-      const metrics = buf.toString("\n");
-      this.sendMetricToGrafana(metrics);
-    } catch (error) {
-      console.log("Error sending metrics", error);
-    }
-  }, period);
-}
+//       const metrics = buf.toString("\n");
+//       this.sendMetricToGrafana(metrics);
+//     } catch (error) {
+//       console.log("Error sending metrics", error);
+//     }
+//   }, period);
+// }
