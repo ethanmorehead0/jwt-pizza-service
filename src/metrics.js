94c662906b165bdb4b37157553718e0ecede81c7
ethan <ethan0@byu.edu>
2025-03-18 10:43:44 -0600

    test metrics.js

--- src/metrics.js
+++ src/metrics.js
@@ -1,87 +1,8 @@
 const os = require("os");
-const config = require("./config");
-
-const requests = {};
-
-function track(endpoint) {
-  return (req, res, next) => {
-    requests[endpoint] = (requests[endpoint] || 0) + 1;
-    next();
-  };
-}
-
-// This will periodically send metrics to Grafana
-const timer = setInterval(() => {
-  Object.keys(requests).forEach((endpoint) => {
-    sendMetricToGrafana("requests", requests[endpoint], { endpoint });
-  });
-}, 10000);
-
-function sendMetricToGrafana(metricName, metricValue, attributes) {
-  attributes = { ...attributes, source: config.source };
-
-  const metric = {
-    resourceMetrics: [
-      {
-        scopeMetrics: [
-          {
-            metrics: [
-              {
-                name: metricName,
-                unit: "1",
-                sum: {
-                  dataPoints: [
-                    {
-                      asInt: metricValue,
-                      timeUnixNano: Date.now() * 1000000,
-                      attributes: [],
-                    },
-                  ],
-                  aggregationTemporality: "AGGREGATION_TEMPORALITY_CUMULATIVE",
-                  isMonotonic: true,
-                },
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  };
-
-  Object.keys(attributes).forEach((key) => {
-    metric.resourceMetrics[0].scopeMetrics[0].metrics[0].sum.dataPoints[0].attributes.push(
-      {
-        key: key,
-        value: { stringValue: attributes[key] },
-      }
-    );
-  });
-
-  fetch(`${config.url}`, {
-    method: "POST",
-    body: JSON.stringify(metric),
-    headers: {
-      Authorization: `Bearer ${config.apiKey}`,
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        console.error("Failed to push metrics data to Grafana");
-      } else {
-        console.log(`Pushed ${metricName}`);
-      }
-    })
-    .catch((error) => {
-      console.error("Error pushing metrics:", error);
-    });
-}
-
-module.exports = { track };
 
 function getCpuUsagePercentage() {
   const cpuUsage = os.loadavg()[0] / os.cpus().length;
-  return cpuUsage.toFixed(2) * 100;
+  return (cpuUsage * 100).toFixed(2);
 }
 
 function getMemoryUsagePercentage() {
@@ -92,20 +13,12 @@ function getMemoryUsagePercentage() {
   return memoryUsage.toFixed(2);
 }
 
-function sendMetricsPeriodically(period) {
-  const timer = setInterval(() => {
-    try {
-      const buf = new MetricBuilder();
-      httpMetrics(buf);
-      systemMetrics(buf);
-      userMetrics(buf);
-      purchaseMetrics(buf);
-      authMetrics(buf);
-
-      const metrics = buf.toString("\n");
-      this.sendMetricToGrafana(metrics);
-    } catch (error) {
-      console.log("Error sending metrics", error);
-    }
-  }, period);
+function reportMetrics() {
+  const cpuUsage = getCpuUsagePercentage();
+  const memoryUsage = getMemoryUsagePercentage();
+  console.log(`CPU Usage: ${cpuUsage}%`);
+  console.log(`Memory Usage: ${memoryUsage}%`);
 }
+
+// Report metrics every 5 seconds
+setInterval(reportMetrics, 5000);
