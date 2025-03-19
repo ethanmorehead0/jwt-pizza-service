31dec0624257125b4359d9c41b022440d4bedc9d
ethan <ethan0@byu.edu>
2025-03-18 21:23:37 -0600

    test

--- src/metrics.js
+++ src/metrics.js
@@ -1,6 +1,6 @@
 const config = require("./config");
 
-const os = require("os");
+// const os = require("os");
 
 // function getCpuUsagePercentage() {
 //   const cpuUsage = os.loadavg()[0] / os.cpus().length;
